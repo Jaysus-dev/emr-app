599a19b01b742d4d7757052f8a3a4db74ae22dd0
jaysus-dev <edg.estoquia2@gmail.com>
2026-01-20 04:19:56 +0800

    ADDED Patient Banner C,R

--- resources/js/Pages/Layouts/PatientLayout.vue
+++ resources/js/Pages/Layouts/PatientLayout.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import Avatar from "@/components/ui/avatar/Avatar.vue";
 import Badge from "@/components/ui/badge/Badge.vue";
+import Button from "@/components/ui/button/Button.vue";
 import Card from "@/components/ui/card/Card.vue";
-import { Copy } from "lucide-vue-next";
+import Separator from "@/components/ui/separator/Separator.vue";
+import {
+    BedSingle,
+    Copy,
+    User,
+    UserCheck,
+    UserCheck2,
+    UserCog,
+    UserSearch,
+} from "lucide-vue-next";
 </script>
 
 <template>
     <Card class="rounded-none border-x-0 bg-primary-foreground shadow-none">
         <div class="flex justify-between">
+            <!--Left-->
             <div class="flex gap-4">
                 <!--image here-->
                 <div class="bg-foreground h-20 w-20 rounded-[100px]" />
@@ -21,40 +32,56 @@ import { Copy } from "lucide-vue-next";
                         class="text-sm text-muted-foreground font-medium flex gap-2 items-center"
                     >
                         Hospital ID: 0000000
-                        <Copy class="h-3 w-3 cursor-pointer" />
+
+                        <Copy class="size-3 cursor-pointer" />
                     </h3>
                     <h3 class="text-lg font-bold">Marcos, Ferdinand S. III</h3>
                 </div>
             </div>
-
-            <div class="grid grid-cols-3 gap-4">
-                <div>col 2.1</div>
-                <div>col 2.2</div>
-                <div>col 2.3</div>
+            <!--Center-->
+            <div class="flex justify-between gap-4 text-sm">
+                <div class="flex flex-col justify-between">
+                    <h3 class="font-medium text-muted-foreground">
+                        Demographics
+                    </h3>
+                    <span class="text-xs font-medium">Gender : Male </span>
+                    <span class="text-xs font-medium">Age : 26</span>
+                    <span class="text-xs font-medium">DOB : Apr 3, 2002</span>
+                </div>
+                <Separator orientation="vertical" />
+                <div class="flex flex-col justify-between">
+                    <h3 class="font-medium text-muted-foreground">
+                        Measurements
+                    </h3>
+                    <span class="text-xs font-medium">Weight : 69 kg </span>
+                    <span class="text-xs font-medium">Height : 172 cm</span>
+                    <span class="text-xs font-medium">BMI : 23.2</span>
+                </div>
+                <Separator orientation="vertical" />
+                <div class="flex flex-col justify-between">
+                    <h3 class="font-bold text-muted-foreground">
+                        Admission Details
+                    </h3>
+                    <span class="text-xs font-medium">Status : Admitted </span>
+                    <span class="text-xs font-medium"
+                        >Admitted: Jan 2, 2025</span
+                    >
+                    <span class="text-xs font-medium"
+                        >Trans-in : Apr 3, 2026</span
+                    >
+                </div>
             </div>
-            <div>col 3</div>
+            <div class="flex flex-col gap-2 max-w-50 w-full">
+                <div class="flex justify-between">
+                    <Button variant="outline"><User /></Button>
+                    <Button variant="outline"><UserSearch /></Button>
+                    <Button variant="outline"><UserCheck2 /></Button>
+                    <Button variant="outline"><UserCog /></Button>
+                </div>
+                <Button variant="outline" class="shadow-none cursor-pointer"
+                    ><BedSingle /> Disposition</Button
+                >
+            </div>
         </div>
     </Card>
-
-    <div class="min-h-screen bg-muted/40">
-        <main class="mx-auto max-w-7xl px-6 py-6">
-            <div class="rounded-2xl bg-background p-6 shadow-sm">
-                Card Header
-                <div class="mb-4 border-b pb-4">
-                    <h2 class="text-xl font-semibold"><slot name="title" /></h2>
-                    <p class="text-sm text-muted-foreground">
-                        <slot name="subtitle" />
-                    </p>
-                </div>
-
-                <!-- 3 Columns -->
-                <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
-                    <section><slot name="col-1" /></section>
-                    <section><slot name="col-2" /></section>
-                    <section><slot name="col-3" /></section>
-                </div>
-                <!-- Tabs -->
-            </div>
-        </main>
-    </div>
 </template>
